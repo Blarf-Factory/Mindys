d9982760ccf054a111136ac3ff1917068a72e4b3
ScribeHollis <jakeawesome12@yahoo.com>
2020-02-16 15:16:14 -0500

    Created Network

---
+++ Assets/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionObject : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+
+        CmdSpawn();
+    }
+
+    public GameObject PlayerUnit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+        
+    }
+
+    GameObject myPlayerUnit;
+
+    [Command]
+    void CmdSpawn()
+    {
+        GameObject player = Instantiate(PlayerUnit);
+
+        myPlayerUnit = player;
+        
+        NetworkServer.SpawnWithClientAuthority(player, connectionToClient);
+    }
+}
