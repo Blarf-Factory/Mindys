be10f375643e869b4c66aa2ae8151ae504cbd8d0
ScribeHollis <jakeawesome12@yahoo.com>
2020-04-08 20:29:18 -0400

    Connected Settings Menu to Object

---
+++ Assets/Scripts/SuperSpatulaScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperSpatulaScreen : MonoBehaviour
+{
+    private Animator animator;
+    private bool viewScreen = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !viewScreen)
+        {
+            animator.SetBool("viewScreen", true);
+            viewScreen = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && viewScreen)
+        {
+            animator.SetBool("viewScreen", false);
+            viewScreen = false;
+        }
+    }
+}
