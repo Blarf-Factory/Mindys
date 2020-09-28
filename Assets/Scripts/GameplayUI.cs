f5c6b0c8f8ae256b8ee9e186bd6c3b12a25cc305
TIm <tberasley@gmail.com>
2020-09-28 19:59:19 -0400

    added pause screen

--- Assets/Scripts/GameplayUI.cs
+++ Assets/Scripts/GameplayUI.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GameplayUI : MonoBehaviour
 {
+    bool paused;
+    public GameObject pausedImage;
+
     // Start is called before the first frame update
     void Start()
     {
+        paused = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Pausing
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Screen.lockCursor = true;
+                paused = false;
+                pausedImage.SetActive(false);
+            }
+            else
+            {
+                Screen.lockCursor = false;
+                paused = true;
+                pausedImage.SetActive(true);
+            }
+
+        }
+
     }
 }
