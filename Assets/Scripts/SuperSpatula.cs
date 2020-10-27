b79e65262ac04849e606e25c9aab4f258e96776f
ScribeHollis <jakeawesome12@yahoo.com>
2020-10-27 11:35:50 -0400

    Compressed SS scripts to one script

--- Assets/Scripts/SuperSpatula.cs
+++ Assets/Scripts/SuperSpatula.cs
@@ -6,63 +6,100 @@ using UnityEngine.SceneManagement;
 public class SuperSpatula : MonoBehaviour
 {
     private GameObject screen;
+    public GameObject MainMenuObject;
+    public GameObject PauseMenuObject;
+    public GameObject StartUpLogo;
     public GameObject StartAnimate;
     public bool ownedByPlayer;
     private Animator animator;
     private bool holding = false;
     private int mode;
+    private AudioSource sound;
+    public AudioClip slide;
+    public AudioClip startSound;
+    private bool viewScreen = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         screen = GameObject.Find("Screen");
+        sound = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
         if(SceneManager.GetActiveScene().name == "MainMenu")
         {
-            mode = 1;
+            if (Input.GetKeyDown(KeyCode.Space) && !holding)
+            {
+                Use();
+                Invoke("changeScreenHelper", 1f);
+                //Invoke("StartUp", 1f);
+            }
+            else if (Input.GetKeyDown(KeyCode.Space) && holding)
+            {
+                changeScreenHelper();
+                Invoke("Use", .25f);
+            }
         }
-        else if(ownedByPlayer)
+        else
         {
-            mode = 0;
+            if (Input.GetKeyDown(KeyCode.Tab) && !holding)
+            {
+                Use();
+                Invoke("changeScreenHelper", 1f);
+            }
+            else if (Input.GetKeyDown(KeyCode.Space) && holding)
+            {
+                changeScreenHelper();
+                Invoke("Use", .25f);
+            }
         }
+    }
 
-        switch (mode)
+    public void changeScreen(int mode)
+    {
+        sound.clip = slide;
+        sound.Play();
+        if(!viewScreen)
         {
-            case 0:
-                if (Input.GetKeyDown(KeyCode.Tab) && !holding)
-                {
-                    Use();
-                    Invoke("changeScreenHelper", 1f);
-                }
-                else if (Input.GetKeyDown(KeyCode.Space) && holding)
-                {
-                    changeScreenHelper();
-                    Invoke("Use", .25f);
-                }
+            openScreen();
+            switch(mode)
+            {
+                //-- MainMenu Mode --//
+                case 0:
+                    bootUp();
                 break;
 
-            case 1:
-                if (Input.GetKeyDown(KeyCode.Space) && !holding)
-                {
-                    Use();
-                    Invoke("changeScreenHelper", 1f);
-                    //Invoke("StartUp", 1f);
-                }
-                else if (Input.GetKeyDown(KeyCode.Space) && holding)
-                {
-                    changeScreenHelper();
-                    Invoke("Use", .25f);
-                }
+                //-- Pause Mode --//
+                case 1:
+                    openPauseMenu();
                 break;
+            }
+        }
+        else
+        {
+            closeScreen();
         }
     }
 
     private void changeScreenHelper()
     {
-        screen.GetComponent<SuperSpatulaScreen>().changeScreen(ownedByPlayer);
+        changeScreen(0);
+    }
+
+    public void openScreen()
+    {
+        screen.GetComponent<SuperSpatulaScreen>().animator.SetBool("viewScreen", true);
+        viewScreen = true;
+        GameObject.Find("ScreenObject").GetComponent<Light>().enabled = true;
+    }
+
+    public void closeScreen()
+    {
+        GameObject.Find("ScreenObject").GetComponent<Light>().enabled = false;
+        screen.GetComponent<SuperSpatulaScreen>().animator.SetBool("viewScreen", false);
+        viewScreen = false;
     }
 
     public void Use()
@@ -82,5 +119,27 @@ public class SuperSpatula : MonoBehaviour
     public void StartUp()
     {
         StartAnimate.SetActive(true);
+    }
+    public void bootUp()
+    {
+        StartUpLogo.SetActive(true);
+        Invoke("startUpSound", 2f);
+        Invoke("OpenMainMenu", 3f);
+    }
+
+    private void OpenMainMenu()
+    {
+        MainMenuObject.SetActive(true);
+    }
+
+    public void startUpSound()
+    {
+        sound.clip = startSound;
+        sound.Play();
+    }
+
+    public void openPauseMenu()
+    {
+        PauseMenuObject.SetActive(true);
     }
 }
--- Assets/Scripts/SuperSpatulaScreen.cs
+++ Assets/Scripts/SuperSpatulaScreen.cs
@@ -4,70 +4,10 @@ using UnityEngine;
 
 public class SuperSpatulaScreen : MonoBehaviour
 {
-    private Animator animator;
-    private bool viewScreen = false;
-    public GameObject MainMenuObject;
-    public GameObject PauseMenuObject;
-    public GameObject StartUpLogo;
-    private bool first;
-
-    private AudioSource sound;
-    public AudioClip slide;
-    public AudioClip startSound;
+    public Animator animator;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        sound = GetComponent<AudioSource>();
-        first = true;
-    }
-
-    public void changeScreen(bool pause)
-    {
-        sound.clip = slide;
-        sound.Play();
-        if (!viewScreen)
-        {
-            animator.SetBool("viewScreen", true);
-            viewScreen = true;
-            GameObject.Find("ScreenObject").GetComponent<Light>().enabled = true;
-            if (first && !pause)
-            {
-                StartUp();
-                first = false;
-            } else if(first && pause)
-            {
-                openPauseMenu();
-            }
-        }
-        else if (viewScreen)
-        {
-            GameObject.Find("ScreenObject").GetComponent<Light>().enabled = false;
-            animator.SetBool("viewScreen", false);
-            viewScreen = false;
-        }
-    }
-
-    public void StartUp()
-    {
-        StartUpLogo.SetActive(true);
-        Invoke("startUpSound", 2f);
-        Invoke("OpenMainMenu", 3f);
-    }
-
-    public void openPauseMenu()
-    {
-        PauseMenuObject.SetActive(true);
-    }
-
-    private void OpenMainMenu()
-    {
-        MainMenuObject.SetActive(true);
-    }
-
-    public void startUpSound()
-    {
-        sound.clip = startSound;
-        sound.Play();
     }
 }
