009c008f87a49699715bc1da153bed255b2ef9f4
ScribeHollis <jakeawesome12@yahoo.com>
2020-11-14 15:08:21 -0500

    added tint to unselected save previews

--- Assets/Scripts/Data Management/GameSavePreview.cs
+++ Assets/Scripts/Data Management/GameSavePreview.cs
@@ -1,9 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSavePreview : MonoBehaviour
 {
+    
+        float r;
+        float g;
+        float b;
+    void Start()
+    {
+        r = GetComponent<Image>().color.r;
+        g = GetComponent<Image>().color.g;
+        b = GetComponent<Image>().color.b;
+    }
+    void Update()
+    {            
+        if( this.gameObject.Equals(GameObject.Find("LoadGameUI").GetComponent<LoadGameUI>().selectedSave) )
+        {
+            GetComponent<Image>().color = new Color(r, g, b, 1f);
+        }
+        else
+        {
+            GetComponent<Image>().color = new Color(r, g, b, 0.5f);
+        }
+    }
     public void SelectSaveDataHelper()
     {
         GameObject.Find("LoadGameUI").GetComponent<LoadGameUI>().SelectSaveData(this.gameObject);
--- Assets/Scripts/Data Management/LoadGameUI.cs
+++ Assets/Scripts/Data Management/LoadGameUI.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class LoadGameUI : MonoBehaviour
 {
-    private GameObject selectedSave;
+    public GameObject selectedSave;
     // Start is called before the first frame update
     void Start()
     {
